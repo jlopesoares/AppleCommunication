36210539d987cb57359d311a911aa00b1e46ac5d
João Pedro Soares <joao.p.soares93@gmail.com>
2023-08-12 14:50:42 +0100

    Initial setup

---
+++ AppleCommunication/Application/AppleCommunicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleCommunicationApp.swift
+//  AppleCommunication
+//
+//  Created by João Pedro Soares on 12/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct AppleCommunicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
